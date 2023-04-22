170a9a5516108756d2d478a62b60fc46b19971a8
Nico Waser <nico.waser@venusnet.ch>
2023-04-22 20:01:44 +0200

    added fairway and tee texture to the visualiser

--- Data_visualisation.py
+++ Data_visualisation.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage.transform import resize
+from math import *
 
 
 class Data_Visualiser(object):
@@ -33,12 +34,55 @@ class Data_Visualiser(object):
         # convert to image
         image = self.colors[poly_data]
 
+        # create fairway / tee texture
+        texture = self.create_diagonal_lines(8, [600, 400])
+        # add texture to fairway
+        image[(texture == 0) & np.all(image == self.colors[3], axis=2)] = [36, 168, 55]
+        image = self.transform_1_to_2(image, 2, [36, 168, 55], self.colors[3])
+        # add texture to tee
+        image[(texture == 0) & np.all(image == self.colors[2], axis=2)] = [114, 171, 70]
+        image = self.transform_1_to_2(image, 2, [114, 171, 70], self.colors[2])
+
         # upscale image
         self.image = resize(image, (900, 600), preserve_range=True, mode='constant').astype(np.int32)
 
         plt.imshow(self.image)
         plt.show()
 
-    # color lookup function
-    def color_lookup(self, index: int):
-        return np.array(self.colors[index], dtype=np.int32)
+    # create fairway / tee texture
+    @staticmethod
+    def create_diagonal_lines(stroke_width: int, dimensions: list[int, int]):
+        # Create a meshgrid of x and y coordinates
+        x, y = np.meshgrid(np.arange(dimensions[1]), np.arange(dimensions[0]))
+
+        # Calculate the distance from the current position to the nearest diagonal line
+        distances = np.floor((x + y) / stroke_width)
+
+        # Set the values of the array to 1 where the distance is odd, and 0 otherwise
+        arr = (distances % 2 == 1).astype(int)
+
+        return arr
+
+    # convert edges of fairway / tee to darker color
+    @staticmethod
+    def transform_1_to_2(arr: np.array, r: int, color_1: np.array, color_2: np.array):
+        # Create a copy of the input array to store the result
+        result = np.copy(arr)
+
+        # Get the dimensions of the input array
+        rows, cols, _ = arr.shape
+
+        # Iterate over each element in the input array
+        for i in range(rows):
+            for j in range(cols):
+                # Check if the current color is color_1
+                if np.all(arr[i, j] == color_1):
+                    # Get the surrounding subarray with radius r
+                    subarray = arr[max(0, i - r):min(rows, i + r + 1), max(0, j - r):min(cols, j + r + 1)]
+
+                    # Check if any color in the surrounding subarray is not color_1 or color_2
+                    if np.any(~(np.all(subarray == color_1, axis=-1) | np.all(subarray == color_2, axis=-1))):
+                        # Replace the color with color_2 in the result array
+                        result[i, j] = color_2
+
+        return result
